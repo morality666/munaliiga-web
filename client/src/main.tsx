dd745e42bd9258c088f9c7b5d44059c4f5735e2c
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2026-04-13 18:45:14 +0300

    Add localization and tanstack router

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,16 @@
-import { App } from "./App.tsx";
 import ReactDOM from "react-dom/client";
+import "./i18n";
+import React from "react";
+import { createRouter, RouterProvider } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
+const router = createRouter({
+  routeTree,
+  scrollRestoration: true,
+});
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+);
